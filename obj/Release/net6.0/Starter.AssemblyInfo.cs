562f85392030edc943f5d506de425f8973ddd548
Ikenna Benedict Nwaka <inwaka39@gmail.com>
2025-09-10 20:23:38 +0200

    update

--- obj/Release/net6.0/Starter.AssemblyInfo.cs
+++ obj/Release/net6.0/Starter.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+959eedacbc4e996a28e4ef02ea216b42cf495321")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
